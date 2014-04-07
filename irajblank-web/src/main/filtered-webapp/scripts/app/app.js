1b773259148719c97fe88db2fbe87a68f24f8668
David Rouyer <rouyer.david@gmail.com>
2014-04-07 15:09:45 +0200

    Clean unnecessary files

--- irajblank-web/src/main/filtered-webapp/scripts/app/app.js
+++ irajblank-web/src/main/filtered-webapp/scripts/app/app.js
@@ -7,12 +7,12 @@ angular.module('blank-iraj', [
 		'raaj-message-interceptor',
 		'raaj-message-service',
 		'iraj-breadcrumbs-service',
-	//	'raaj-progress-interceptor',
+		'raaj-progress-interceptor',
 		'raaj-table-service'
 	])
-//	.config(function (irajProgressInterceptorProvider) {
-//		irajProgressInterceptorProvider.setDefaultMessage('Chargement en cours...');
-//	})
+	.config(function (irajProgressInterceptorProvider) {
+		irajProgressInterceptorProvider.setDefaultMessage('Chargement en cours...');
+	})
 	.config(function (irajMessageServiceProvider) {
 		irajMessageServiceProvider.setBootstrapVersion('3.x');
 	})

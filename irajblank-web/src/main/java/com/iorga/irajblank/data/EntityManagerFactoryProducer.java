122701b4a79f1a2332ef739272112d277e6aee1c
Anthony OGIER <aogier@iorga.com>
2014-04-25 18:02:19 +0200

    Workaround for https://hibernate.atlassian.net/browse/HHH-8818

--- irajblank-web/src/main/java/com/iorga/irajblank/data/EntityManagerFactoryProducer.java
+++ irajblank-web/src/main/java/com/iorga/irajblank/data/EntityManagerFactoryProducer.java
@@ -1,19 +1,40 @@
 package com.iorga.irajblank.data;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.sql.DataSource;
+
+import org.hibernate.cfg.Environment;
+import org.hibernate.jpa.boot.internal.ParsedPersistenceXmlDescriptor;
+import org.hibernate.jpa.boot.internal.PersistenceXmlParser;
 
 @ApplicationScoped
 public class EntityManagerFactoryProducer {
 
 	@Produces @ApplicationScoped
-	public EntityManagerFactory createEntityManagerFactory() {
-		return Persistence.createEntityManagerFactory("com.iorga.irajblank");
+	public EntityManagerFactory createEntityManagerFactory() throws NamingException {
+		// workaround for https://hibernate.atlassian.net/browse/HHH-8818
+		List<ParsedPersistenceXmlDescriptor> persistenceUnits = PersistenceXmlParser.locatePersistenceUnits(new HashMap<>());
+		if (persistenceUnits.size() > 1) {
+			throw new IllegalStateException("There is more than one persistence unit. Aborting.");
+		}
+		String dataSourceJndiLocation = (String) persistenceUnits.get(0).getNonJtaDataSource();
+		DataSource dataSource = (DataSource) new InitialContext().lookup(dataSourceJndiLocation);
+		Map<String, Object> props = new HashMap<>();
+		props.put(Environment.DATASOURCE, dataSource);
+
+		return Persistence.createEntityManagerFactory("com.iorga.irajblank", props);
 	}
 
 	public void disposeEntityManagerFactory(@Disposes final EntityManagerFactory entityManagerFactory) {
